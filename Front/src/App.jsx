847170c4aca817b811e650fb337d4c4ac8db2c15
PARCERO <santiago.ago2002@gmail.com>
2024-12-25 14:08:08 -0500

    empaquetador con vite para tailwind

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { initialPage } from './components/initialPage';
+import { initialPage } from '../src/components/initialPage';
 
 function App() {
   return (

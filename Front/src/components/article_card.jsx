f7d474b92afd3d785c53eaa69f2bd2bd96c5cb1e
Wilmer <wilmer.pardo@unl.edu.ec>
2025-01-24 15:54:13 -0500

    Icono carro

--- Front/src/components/article_card.jsx
+++ Front/src/components/article_card.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
 import { IoIosArrowDropright } from "react-icons/io";
+import { FaCartPlus } from "react-icons/fa";
 
 const ArticleCard = ({ codigoArticulo, descripcion, marca, modelo, precio, imgUrl }) => {
     return (
-        <div className='w-[300px] h-[440px] neumorphism-tarjeta mt-14 flex flex-col transition duration-300 hover:scale-105 hover-button'>
-            <div className='flex-1'>
-                <div className='flex w-full h-full justify-center items-center'>
-                    <div className='w-[80%] h-[85%] rounded-lg'>
-                        <img className='w-full h-full rounded-lg' src={imgUrl} alt={`${marca} ${modelo}`} />
+        <div className="w-[300px] h-[440px] neumorphism-tarjeta mt-14 flex flex-col transition duration-300 hover:scale-105 hover-button">
+            <div className="flex-1">
+                <div className="flex w-full h-full justify-center items-center">
+                    <div className="w-[80%] h-[85%] rounded-lg">
+                        <img
+                            className="w-full h-full object-cover rounded-lg"
+                            src={imgUrl}
+                            alt={`${descripcion}`}
+                        />
                     </div>
                 </div>
             </div>
-            <div className='flex-1'>
-                <div className='flex w-full h-full justify-center items-center'>
-                    <div className='flex flex-col w-[90%] h-[90%]'>
-                        <div className='flex items-center w-full h-[50px]  px-2'>
-                            <p className='font-semibold text-white text-lg border-b-2'>{marca} <strong className='text-[#3399ff]'>{modelo}</strong></p>
-                        </div>
-                        <div className='w-full h-[100px] p-2 '>
-                            <p className='text-white text-sm custom-font line-clamp-3'>
-                                {descripcion}
-                            </p>
-                        </div>
-                        <div className='flex items-center justify-between w-full h-[35px] text-white'>
-                            <div className='h-full flex items-center justify-center px-2'>
-                                <p className='text-lg'><strong className='text-[#3399ff]'>Precio:</strong> ${precio}</p>
-                            </div>
-                            <div className='h-full flex items-end justify-center text-5xl text-[#3399ff]'>
-                                <IoIosArrowDropright className='transition duration-300 hover:text-white' />
-                            </div>
-                        </div>
+            <div className="w-full flex flex-col gap-3 items-center p-3">
+                <div className="w-full flex flex-col gap-2">
+                    <div className="w-full flex justify-between">
+                        <span className="font-bold text-sm">{marca}</span>
+                        <span className="text-xs">{modelo}</span>
                     </div>
+                    <div className="text-xs">{descripcion}</div>
                 </div>
+                <div className="w-full flex justify-between items-center">
+                    <span className="font-bold text-xs">$ {precio}</span>
+                    <button className="text-2xl neumorphism-button p-2">
+                        <IoIosArrowDropright />
+                    </button>
+                </div>
+                <button className="neumorphism-button px-3 py-1 flex gap-2 items-center mt-2">
+                    Agregar <FaCartPlus />
+                </button>
             </div>
         </div>
     );
-}
+};
 
 export default ArticleCard;

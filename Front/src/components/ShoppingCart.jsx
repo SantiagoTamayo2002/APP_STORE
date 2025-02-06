1a7cf96e0b0a2dee33a68c596486cce1a8b1468f
Ariel Tandazo <ariel.a.tandazo@unl.edu.ec>
2025-02-06 14:24:32 -0500

    Había una letra mal N en ./navbar

--- Front/src/components/ShoppingCart.jsx
+++ Front/src/components/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import Footer from './Footer';
 import ArticleCard from './articles';
 

411c7ace7d843d99b6135d3bf8a741878194b712
Jasiel <jasiel.quezada@unl.edu.ec>
2025-02-07 01:14:37 -0500

    horizontal interface

--- Front/src/components/offers.jsx
+++ Front/src/components/offers.jsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Navbar from './navbar';
 import Footer from './Footer';
 import '../App.css';
 import { FaCartPlus } from "react-icons/fa";
+import { IoIosArrowDropright } from "react-icons/io";
 
 function Offers() {
     const [offers, setOffers] = useState([]);
+    const [isDetailVisible, setIsDetailVisible] = useState(false);
+    const [selectedArticle, setSelectedArticle] = useState(null); // Mantener el artículo seleccionado para mostrar los detalles
+    const detailRef = useRef(null);
 
     useEffect(() => {
         fetch('http://localhost:5000/get_offers') // Ruta para obtener las ofertas
@@ -42,13 +46,36 @@ function Offers() {
         .catch((error) => console.error("Error al agregar al carrito:", error));
     };
 
+    const handleDetailToggle = (article) => {
+        setSelectedArticle(article);
+        setIsDetailVisible(!isDetailVisible);
+    };
+
+    const handleClickOutside = (event) => {
+        if (detailRef.current && !detailRef.current.contains(event.target)) {
+            setIsDetailVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isDetailVisible) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isDetailVisible]);
+
     return (
         <div className="overflow-hidden font-sans bg-[#101828] text-white">
             <Navbar />
             <div className="bg-[#101828] flex justify-center py-10 min-h-screen mt-16">
-                <div className="flex flex-wrap w-full px-6 justify-center gap-6">
+                <div className="flex flex-nowrap overflow-x-auto w-full px-6 gap-6">
                     {offers.map((offer) => (
-                        <div key={offer.id_oferta} className="offer-card neumorphism-tarjeta p-6 mb-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                        <div key={offer.id_oferta} className="offer-card neumorphism-tarjeta p-6 min-w-[300px] flex-grow">
                             <h2 className="offer-name text-xl font-semibold text-[#3399ff]">{offer.nombre}</h2>
                             <p className="offer-type text-sm text-gray-500"> {offer.tipo}</p>
                             <p className="offer-value text-lg font-bold text-[#3399ff]">Descuento: {offer.valor} %</p>
@@ -60,7 +87,7 @@ function Offers() {
                                 {offer.articulos?.map((article) => (
                                     <li key={article.codigo_articulo} className="offer-article mb-4 bg-gray-600 p-4 rounded-lg shadow-md transition-all hover:z-50">
                                         <div className="article-details">
-                                            <p className="text-sm text-gray-200">Descripción: {article.descripcion}</p>
+                                            <p className="text-sm text-gray-200">{article.descripcion}</p>
                                             <p className="text-sm text-gray-400">Precio Original: <span className="line-through text-red-400">${article.precio_original}</span></p>
                                             <p className="text-sm text-green-400 font-semibold">Precio con Descuento: ${article.precio_oferta}</p>
                                             <p className="text-sm text-yellow-300">Descuento: {article.descuento_porcentaje}%</p>
@@ -73,22 +100,14 @@ function Offers() {
                                             )}
                                         </div>
                                         <div className="flex gap-2 items-center mt-2">
-                                            <select
-                                                className="neumorphism-button px-3 py-1 bg-gray-200"
-                                                onChange={(e) => article.cantidad = parseInt(e.target.value)}
+                                        
+                                            
+                                            <div
+                                                className='h-full flex items-end justify-center px-4 py-1 text-2xl text-[#3399ff] cursor-pointer'
+                                                onClick={() => handleDetailToggle(article)}
                                             >
-                                                {[...Array(10).keys()].map((num) => (
-                                                    <option key={num + 1} value={num + 1}>
-                                                        {num + 1}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                            <button 
-                                                className="neumorphism-button px-3 py-1 flex gap-2 items-center"
-                                                onClick={() => handleAddToCart(article.codigo_articulo, article.cantidad || 1)}
-                                            >
-                                                Confirmar <FaCartPlus />
-                                            </button>
+                                                <IoIosArrowDropright className='transition duration-300 hover:text-white' />
+                                            </div>
                                         </div>
                                     </li>
                                 ))}
@@ -97,6 +116,69 @@ function Offers() {
                     ))}
                 </div>
             </div>
+            {isDetailVisible && selectedArticle && (
+                <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-filter backdrop-blur-lg transition-opacity duration-100'>
+                    <div ref={detailRef} className='w-[70%] h-[500px] neumorphism-tarjeta p-4 transition-transform duration-100 transform scale-100'>
+                        <div className='flex flex-col h-full'>
+                            <div className='flex justify-end'>
+                                <button
+                                    className='text-2xl text-[#3399ff]'
+                                    onClick={() => setIsDetailVisible(false)}
+                                >
+                                    &times;
+                                </button>
+                            </div>
+                            <div className='flex items-center h-full'>
+                                <div className='flex w-[40%] h-full'>
+                                    <img 
+                                        className='w-[90%] h-[75%] rounded-lg' 
+                                        src={selectedArticle.url_img} 
+                                        alt={selectedArticle.descripcion} 
+                                    />
+                                </div>
+                                <div className='flex w-[60%] h-full items-center'>
+                                    <div className='w-full'>
+                                        <div className='h-[60px] px-2'>
+                                            <p className='font-semibold text-white text-3xl border-b-2 inline'>
+                                                {selectedArticle.marca} <strong className='text-[#3399ff]'>{selectedArticle.modelo}</strong>
+                                            </p>
+                                        </div>
+                                        <div className='w-full h-[270px] p-2'>
+                                            <p className='text-white text-base custom-font mb-3'>
+                                                {selectedArticle.descripcion}
+                                            </p>
+                                        </div>
+                                        <div className='w-full mt-4 h-[50px] flex justify-between items-center p-2  px-3'>
+                                            <p className='text-2xl text-white'>
+                                                <strong className='text-[#3399ff]'>Precio:</strong> <strong>${selectedArticle.precio_oferta}</strong>
+                                            </p>
+                                            <div className='flex items-center px-1'>
+                                                <select
+                                                    className="px-3 py-1 bg-gray-600 text-white rounded-lg mr-2"
+                                                    value={selectedArticle.cantidad}
+                                                    onChange={(e) => setCantidad(parseInt(e.target.value))}
+                                                >
+                                                    {[...Array(10).keys()].map((num) => (
+                                                        <option key={num + 1} value={num + 1}>
+                                                            {num + 1}
+                                                        </option>
+                                                    ))}
+                                                </select>
+                                                <button
+                                                    className="text-xl px-3 py-1 flex gap-2 items-center text-white bg-[#3399ff] rounded-lg font-semibold"
+                                                    onClick={() => handleAddToCart(selectedArticle.codigo_articulo, selectedArticle.cantidad || 1)}
+                                                >
+                                                    Confirmar <FaCartPlus />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
             <Footer />
         </div>
     );

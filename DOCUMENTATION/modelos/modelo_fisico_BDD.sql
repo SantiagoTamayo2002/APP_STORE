c750c9f20b8e1c415b74cb9108e7f017728e59bc
Santiago Tamayo <manuel.tamayo@unl.edu.ec>
2025-02-07 01:18:39 -0500

    trigger ans views

--- DOCUMENTATION/modelos/modelo_fisico_BDD.sql
+++ DOCUMENTATION/modelos/modelo_fisico_BDD.sql
@@ -7,7 +7,7 @@ CREATE TABLE usuario (
     nom_apellido      VARCHAR(250) NOT NULL,
     rol               VARCHAR(20)  NOT NULL,
     cuenta_correo     VARCHAR(250) NOT NULL UNIQUE,
-    cuenta_contrasena VARCHAR(20) NOT NULL
+    cuenta_contrasena VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE direccion (
@@ -102,3 +102,60 @@ CREATE TABLE detalle_oferta (
     FOREIGN KEY (id_oferta) REFERENCES oferta(id_oferta),
     FOREIGN KEY (id_articulo) REFERENCES articulo(codigo_articulo)
 );
+
+
+CREATE VIEW vista_pedidos_usuarios AS
+SELECT 
+    p.n_pedido,
+    u.dni,
+    u.nom_nombre,
+    u.nom_apellido,
+    u.cuenta_correo,
+    p.codigo_pago,
+    f.n_factura,
+    f.fecha,
+    d.calle_primaria,
+    d.ciudad,
+    d.provincia
+FROM 
+    pedido p
+JOIN 
+    usuario u ON p.dni_usuario = u.dni
+JOIN 
+    factura f ON p.num_factura = f.n_factura
+JOIN 
+    direccion d ON u.dni = d.dni_usuario;
+
+CREATE VIEW vista_inventario_oferta AS
+SELECT 
+    a.codigo_articulo,
+    a.descripcion,
+    a.precio,
+    a.marca,
+    a.modelo,
+    i.cantidad AS cantidad_en_inventario,
+    o.nombre AS oferta_nombre,
+    o.tipo AS oferta_tipo,
+    o.valor AS oferta_valor,
+    o.fecha_inicio,
+    o.fecha_fin
+FROM 
+    articulo a
+JOIN 
+    inventario i ON a.codigo_inventario = i.codigo_inventario
+JOIN 
+    detalle_oferta do ON a.codigo_articulo = do.id_articulo
+JOIN 
+    oferta o ON do.id_oferta = o.id_oferta;
+    
+    
+CREATE TRIGGER actualizar_inventario_despues_pedido
+AFTER INSERT ON detalle_pedido
+FOR EACH ROW
+BEGIN
+    UPDATE inventario
+    SET cantidad = cantidad - NEW.N_articulos
+    WHERE codigo_inventario = (SELECT codigo_inventario FROM articulo WHERE codigo_articulo = NEW.codigo_articulo);
+END;
+
+
